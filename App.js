40108914d48139a004a69ef72f252ed0d67caf60
René Wilby <rene.wilby@timebook.com>
2021-10-21 15:48:17 +0200

    add oauth authentication example using github

--- App.js
+++ App.js
@@ -1,21 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
+import { CLIENT_ID, CLIENT_SECRET } from "@env";
 
-export default function App() {
+// Endpoint
+const discovery = {
+  authorizationEndpoint: "https://github.com/login/oauth/authorize",
+  tokenEndpoint: "https://github.com/login/oauth/access_token",
+  revocationEndpoint: `https://github.com/settings/connections/applications/${CLIENT_ID}`,
+};
+
+const App = () => {
+  const [sessionCode, setSessionCode] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
+  const [user, setUser] = useState({});
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: CLIENT_ID,
+      scopes: ["read:user"],
+      redirectUri: makeRedirectUri({
+        path: "oauth/github",
+      }),
+    },
+    discovery
+  );
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { code: sessionCode } = response.params;
+      setSessionCode(sessionCode);
+    } else if (response?.type === "error") {
+      console.log("Authentication error: ", response.error);
+    }
+  }, [response]);
+
+  useEffect(() => {
+    if (sessionCode !== null) {
+      fetch("https://github.com/login/oauth/access_token", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          client_id: CLIENT_ID,
+          client_secret: CLIENT_SECRET,
+          code: sessionCode,
+        }),
+      }).then((response) => {
+        response.json().then(({ access_token: accessToken }) => {
+          setAccessToken(accessToken);
+        });
+      });
+    }
+  }, [sessionCode]);
+
+  useEffect(() => {
+    if (accessToken !== null) {
+      fetch("https://api.github.com/user", {
+        headers: {
+          Authorization: `token ${accessToken}`,
+        },
+      }).then((response) => {
+        response.json().then((data) => {
+          if (data?.type === "User") {
+            setUser(data);
+          }
+        });
+      });
+    }
+  }, [accessToken]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {!user?.name && (
+        <Button
+          disabled={!request}
+          onPress={() => {
+            promptAsync();
+          }}
+          title={"Sign in with GitHub"}
+        />
+      )}
+      {user?.name && <Text>{`Hello ${user?.name}!`}</Text>}
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
--- README.md
+++ README.md
@@ -7,6 +7,9 @@ Die App basiert auf React Native und Expo.
 ## Installationsanleitung
 
 1. Repository clonen
-2. In das Projektverzeichnis wechseln
-3. `npm install` ausführen
-4. `npm start` ausführen
+2. GitHub OAuth App erstellen (https://github.com/settings/developers). Dabei `exp://127.0.0.1:19000/--/oauth/github` als im Feld `Authorization callback URL` hinterlegen.
+3. `.env` Datei im Projektverzeichnis erstellen
+4. `CLIENT_ID` und `CLIENT_SECRET` der GitHub OAuth App in `.env` Datei hinterlegen
+5. In das Projektverzeichnis wechseln
+6. `npm install` ausführen
+7. `npm start` ausführen
